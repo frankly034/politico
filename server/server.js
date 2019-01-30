46376eb39678ce8a4f88f5efcfee9d031570dc16
frankly034 <franklynugege@gmail.com>
2019-01-30 21:08:02 +0100

    feat(server): implement POST /offices
    - setup the server to listen for a post request to /offices
    - add a new government office entry to dummy data

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import officeRoute from './routes/officeRoutes';
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -7,4 +8,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(port);
+const apiUrlVersion1 = '/api/v1';
+
+
+app.use(apiUrlVersion1, officeRoute);
+if (!module.parent) {
+  app.listen(port);
+}
+
+export default app;
