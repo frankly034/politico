305916e10cde9bf56581d22d93eaa33a79504f50
frankly034 <franklynugege@gmail.com>
2019-01-30 12:34:39 +0100

    feat(POST /parties) implement create parties api
    - adds partyRoutes to server.js
    - created express middleware for the route
    [Delivers #163586540]

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import partyRoute from './routes/partyRoutes';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(partyRoute);
 
 app.listen(port);
