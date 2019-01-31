a31a4e2214cc9a62259c04f6a6de27d4c6adc511
frankly034 <franklynugege@gmail.com>
2019-01-31 04:48:28 +0100

    bug(rebase): resolved conflicts
    - resolve merge conflicts
    - refactor middlewares to return error response instead of sending to
    controller
    - versioned the party route
    [Delivers #163586540]

--- server/middlewares/partyMiddleware.js
+++ server/middlewares/partyMiddleware.js
@@ -2,9 +2,10 @@ class PartyMiddleware {
   static create(req, res, next) {
     const { name, hqAddress, logoUrl } = req.body;
     if (!name || !hqAddress || !logoUrl) {
-      req.error = { msg: 'name, hqAddress and logoUrl are required', status: 400 };
+      const error = { msg: 'name, hqAddress and logoUrl are required', status: 400 };
+      return res.status(400).send(error);
     }
-    next();
+    return next();
   }
 }
 
--- server/routes/partyRoutes.js
+++ server/routes/partyRoutes.js
@@ -4,8 +4,6 @@ import PartyMiddleware from '../middlewares/partyMiddleware';
 
 const router = express.Router();
 
-const apiStr = '/api/v1/parties';
-
-router.post(apiStr, PartyMiddleware.create, PartyController.createParty);
+router.post('/parties', PartyMiddleware.create, PartyController.createParty);
 
 export default router;
