a7a0d363d8d5f696c2572a27f002b05c0928a14c
frankly034 <franklynugege@gmail.com>
2019-01-31 22:03:58 +0100

    chore(model): add editAParty()
    - ensures that the party exist, then updates the name
    - if not found returns null
    [Delivers #163625014]

--- server/models/partyModel.js
+++ server/models/partyModel.js
@@ -28,6 +28,20 @@ class PartyModel {
     const returnedParties = parties.filter(party => party.id !== parseInt(id, 10));
     return returnedParties.length < parties.length ? 1 : 0;
   }
+
+  static editAParty(id, name) {
+    let party = PartyModel.getAParty(id);
+    if (party.length !== 0) {
+      PartyModel.findAll().forEach((item) => {
+        if (item.id === parseInt(id, 10)) {
+          item.name = name;
+          party = item;
+        }
+      });
+      return party;
+    }
+    return null;
+  }
 }
 
 export default PartyModel;
