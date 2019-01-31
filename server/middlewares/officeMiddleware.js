1467b9c3f375559d7587f85d994f4443132a0139
frankly034 <franklynugege@gmail.com>
2019-01-31 10:45:43 +0100

    chore(middleware): add getAnOffice()
    - ensures the type of data passed in is a Number
    [Delivers #163616343]

--- server/middlewares/officeMiddleware.js
+++ server/middlewares/officeMiddleware.js
@@ -7,6 +7,15 @@ class OfficeMiddleware {
     }
     return next();
   }
+
+  static getAnOffice(req, res, next) {
+    const { id } = req.params;
+    if (typeof Number(id) !== 'number') {
+      const error = { msg: 'Bad request', status: 400 };
+      return res.status(400).send(error);
+    }
+    return next();
+  }
 }
 
 export default OfficeMiddleware;
