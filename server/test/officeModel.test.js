d8633ea937d4cd381e9c96ee8339f90a1cfdd1c3
frankly034 <franklynugege@gmail.com>
2019-01-31 06:08:24 +0100

    feat(test): add test feature
    - should return array object if successful
    - should return status 200 if successful
    [Delivers #163613369]

--- server/test/officeModel.test.js
+++ server/test/officeModel.test.js
@@ -3,32 +3,45 @@ import request from 'supertest';
 
 import app from '../server';
 
-import parties from '../models/officeData';
+import offices from '../models/officeData';
 
 const { expect } = chai;
-const apiStr = '/api/v1/offices';
+const apiUrl = '/api/v1/offices';
 
 describe('POST /offices', () => {
   it('create() should create a new office', (done) => {
     const office = { name: 'senator', type: 'federal' };
 
     request(app)
-      .post(apiStr)
+      .post(apiUrl)
       .send(office)
       .expect(200)
       .expect(res => {
         const { name, type, id } = res.body.data;
         const returnedOffice = { id, name, type };
-        expect(returnedOffice.id).to.eq(parties.length);
+        expect(returnedOffice.id).to.eq(offices.length);
       })
       .end((err, res) => { if (err) done(err); else done(); });
   });
 
   it('should not create a office with incomplete data', (done) => {
     request(app)
-      .post(apiStr)
+      .post(apiUrl)
       .send({})
       .expect(400)
       .end((err, res) => { if (err) done(err); else done(); });
+  });
+});
+
+describe('GET /offices', () => {
+  it('should get all offices', (done) => {
+    request(app)
+      .get(apiUrl)
+      .expect(200)
+      .expect((res) => {
+        const body = res.body.data;
+        expect(body.length).to.equal(offices.length);
+      })
+      .end(done);
   });
 });
