45d608088898ad293a398a45d2883e8af8c9e5e1
frankly034 <franklynugege@gmail.com>
2019-01-31 13:19:29 +0100

    feat(route): add GET /parties/:id
    - makes the route available for comsumption
    [Delivers #163620127]

--- server/routes/partyRoutes.js
+++ server/routes/partyRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import PartyController from '../controllers/partyController';
-import PartyMiddleware from '../middlewares/partyMiddleware';
+import PartyMiddleware from '../middlewares/PartyMiddleware';
 
 const router = express.Router();
 
 router.post('/parties', PartyMiddleware.create, PartyController.createParty);
+router.get('/parties/:id', PartyMiddleware.getAParty, PartyController.getAParty);
 
 export default router;
