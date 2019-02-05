c949fdae400e1ec0efbee48e2ba694439a3b7772
frankly034 <franklynugege@gmail.com>
2019-02-05 12:25:24 +0100

    chore(Db Connector): create connection to the db
    - uses env variables to create a pool connection to the database
    - returns the pg pool query function
    [Delivers #163669011]

---
+++ server/models/db/connector.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config();
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+pool.on('connect', () => {
+  console.log('connected to the database');
+});
+
+const query = (sql, params) => {
+  return new Promise((resolve, reject) => {
+    pool.query(sql, params)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((e) => {
+        reject(e);
+      });
+  });
+};
+
+export default query;
