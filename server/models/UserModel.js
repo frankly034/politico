de0688835b2704814d0669bacd8136af3759ec37
frankly034 <franklynugege@gmail.com>
2019-02-05 12:18:20 +0100

    chore(UserModel): create user model
    - create user model with constructor
    - create setHashPassword using bcryptjs
    [Delivers #163669011]

---
+++ server/models/UserModel.js
@@ -0,0 +1,34 @@
+import bcrypt from 'bcryptjs';
+
+import TokenModel from './TokenModel';
+import query from './db/connector';
+
+class UserModel {
+  constructor(user) {
+    this.id = user.id || null;
+    this.firstname = user.firstname;
+    this.lastname = user.lastname;
+    this.othername = user.othername;
+    this.email = user.email;
+    this.phoneNumber = user.phoneNumber || user.phonenumber;
+    this.passportUrl = user.passportUrl || user.passporturl;
+    this.phonenumber = user.phoneNumber || user.phonenumber;
+    this.passporturl = user.passportUrl || user.passporturl;
+    this.password = user.password;
+    this.isAdmin = user.isAdmin || user.isadmin;
+  }
+
+  setHashPassword() {
+    const user = this;
+    return new Promise((resolve, reject) => {
+      bcrypt.genSalt(10).then(salt => bcrypt.hash(user.password, salt)
+        .then((hash) => {
+          user.password = hash;
+          return resolve(user);
+        })
+        .catch(() => reject()));
+    });
+  }
+}
+
+export default UserModel;
