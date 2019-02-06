c979571aa6ef9b439eff9acd7d7c32628f4366af
frankly034 <franklynugege@gmail.com>
2019-02-07 00:44:38 +0100

    chore(DB): update table schema
    - add schema for:
    - create candidates table
    - create votes table
    [Delivers #163777042]

--- server/models/db/schemas/index.js
+++ server/models/db/schemas/index.js
@@ -1,16 +1,25 @@
 import query from '../connector';
 
-const userTable = 'CREATE TABLE IF NOT EXISTS users ( id SERIAL PRIMARY KEY, firstname VARCHAR (50) NOT null, lastname VARCHAR (50) NOT null, othername VARCHAR (50), email VARCHAR (60) UNIQUE NOT NULL, phonenumber VARCHAR (14) NOT null, passporturl VARCHAR (255) NOT null, password VARCHAR (255) NOT NULL, isAdmin BOOLEAN DEFAULT FALSE)';
+const table = `
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS tokens CASCADE;
+DROP TABLE IF EXISTS parties CASCADE;
+DROP TABLE IF EXISTS offices CASCADE;
+DROP TABLE IF EXISTS candidates CASCADE;
+DROP TABLE IF EXISTS votes CASCADE;
 
-const tokenTable = 'CREATE TABLE IF NOT EXISTS tokens ( id SERIAL PRIMARY KEY, token TEXT NOT null, access VARCHAR (10) NOT null, user_id INTEGER REFERENCES users (id) ON DELETE CASCADE)';
+CREATE TABLE IF NOT EXISTS users ( id SERIAL PRIMARY KEY, firstname VARCHAR (50) NOT null, lastname VARCHAR (50) NOT null, othername VARCHAR (50), email VARCHAR (60) UNIQUE NOT NULL, "phoneNumber" VARCHAR (14) NOT null, "passportUrl" VARCHAR (255) NOT null, password VARCHAR (255) NOT NULL, "isAdmin" BOOLEAN DEFAULT FALSE);
 
-const partyTable = 'CREATE TABLE IF NOT EXISTS parties ( id SERIAL PRIMARY KEY, name VARCHAR (50) NOT null, hqaddress TEXT NOT null, logourl VARCHAR (255) NOT null)';
+CREATE TABLE IF NOT EXISTS tokens ( id SERIAL PRIMARY KEY, token TEXT NOT null, access VARCHAR (10) NOT null, user_id INTEGER REFERENCES users (id) ON DELETE CASCADE);
 
-const officeTable = 'CREATE TABLE IF NOT EXISTS offices ( id SERIAL PRIMARY KEY, name VARCHAR (100) NOT null, type VARCHAR (50) NOT null)';
+CREATE TABLE IF NOT EXISTS parties ( id SERIAL PRIMARY KEY, name VARCHAR (50) NOT null, "hqAddress" TEXT NOT null, "logoUrl" VARCHAR (255) NOT null);
 
-query(userTable)
-  .then(() => query(tokenTable))
-  .then(() => query(partyTable))
-  .then(() => query(officeTable))
+CREATE TABLE IF NOT EXISTS offices ( id SERIAL PRIMARY KEY, name VARCHAR (100) NOT null, type VARCHAR (50) NOT null);
+
+CREATE TABLE IF NOT EXISTS candidates ( id SERIAL, candidate int UNIQUE REFERENCES users (id), office int REFERENCES offices (id), party INTEGER REFERENCES parties (id), confirmed BOOLEAN DEFAULT FALSE, PRIMARY KEY (candidate, office));
+
+CREATE TABLE IF NOT EXISTS votes (id SERIAL, office int REFERENCES offices (id), candidate int REFERENCES candidates (candidate), "createdBy" int REFERENCES users (id), "createdOn" Timestamp DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (office, "createdBy"));`;
+
+query(table)
   .then(() => console.log('tables created'))
   .catch(e => console.log(e));
