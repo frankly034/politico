b0b5216034c2e79284eed25f0a5838161eda5855
frankly034 <franklynugege@gmail.com>
2019-02-06 19:27:24 +0100

    chore(controller): add loginUser()
    - function to handle loginUser
    - saves auth-token if found
    [Delivers #163742801]

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -2,6 +2,7 @@ import { uploader } from 'cloudinary';
 import UserModel from '../models/UserModel';
 import cloudinaryConfig from '../config/cloudinaryConfig';
 import fetchFormattedImage from '../config/datauriConfig';
+import TokenModel from '../models/TokenModel';
 
 class UserController {
   static createUser(req, res) {
@@ -18,7 +19,28 @@ class UserController {
       })
       .then(() => UserModel.createUser(body))
       .then(result => res.status(201).send({ status: 201, token: result.token, data: result.user }))
-      .catch(() => res.status(500).send({ status: 500, message: 'Internal server error.' }));
+      .catch(() => res.status(500).send({ status: 500, message: 'Internal server error' }));
+  }
+
+  static loginUser(req, res) {
+    const { email, password } = req.body;
+    UserModel.loginUser(email, password)
+      .then((user) => {
+        TokenModel.saveToken(user, 'auth')
+          .then(token => res.status(200)
+            .header('x-auth', token.token)
+            .send({
+              status: 200,
+              data: {
+                token: token.token,
+                user,
+              },
+            }));
+      })
+      .catch(() => res.status(404).send({
+        status: 404,
+        message: 'User not found',
+      }));
   }
 }
 
