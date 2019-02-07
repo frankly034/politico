0aaacdff75dd288bf5a821a81154d7cd0df66142
frankly034 <franklynugege@gmail.com>
2019-02-07 01:08:19 +0100

    bug(response): edit response message
    [Delivers #163777042]

--- server/controllers/CandidateController.js
+++ server/controllers/CandidateController.js
@@ -6,30 +6,30 @@ class CandidateController {
       .then(createdCandidate => res.status(201).send({ status: 201, data: createdCandidate }))
       .catch((e) => {
         if (e.code > 23500) {
-          return res.status(400).send({ status: 400, message: e.detail });
+          return res.status(400).send({ status: 400, error: e.detail });
         }
-        return res.status(500).send({ status: 500, message: 'Server error, please try again.' });
+        return res.status(417).send({ status: 417, error: 'Excpectation failed.' });
       });
   }
 
   static getAllCandidates(req, res) {
     CandidateModel.getAllCandidates()
       .then(candidates => res.status(200).send({ status: 200, data: candidates }))
-      .catch(() => res.status(404).send({ status: 404, message: 'No candidates found.' }));
+      .catch(() => res.status(417).send({ status: 417, error: 'No candidates found.' }));
   }
 
   static getACandidate(req, res) {
     const { id } = req.params;
     CandidateModel.getACandidate(id)
       .then(returnedCandidate => res.status(200).send({ status: 200, data: returnedCandidate }))
-      .catch(() => res.status(404).send({ status: 404, message: 'Candidate not found.' }));
+      .catch(() => res.status(404).send({ status: 404, error: 'Candidate not found.' }));
   }
 
   static getAllCandidatesByOffice(req, res) {
     const { id } = req.params;
     CandidateModel.getAllCandidatesByOffice(id)
       .then(candidates => res.status(200).send({ status: 200, data: candidates }))
-      .catch(() => res.status(404).send({ status: 404, message: 'No candidates found for this office.' }));
+      .catch(() => res.status(404).send({ status: 404, error: 'No candidates found for this office.' }));
   }
 }
 
