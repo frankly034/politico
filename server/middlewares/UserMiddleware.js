1ce869ec1151d7fc35390cb4d2692b9ed7007a93
frankly034 <franklynugege@gmail.com>
2019-02-08 00:34:15 +0100

    edit unauthorisation error message

--- server/middlewares/UserMiddleware.js
+++ server/middlewares/UserMiddleware.js
@@ -79,9 +79,9 @@ class UserMiddleware {
         if (user.isAdmin) {
           return next();
         }
-        return res.status(401).send({ status: 401, error: 'You do not have permission to access this.' });
+        return res.status(401).send({ status: 401, error: 'You do not have permission to access this resource.' });
       })
-      .catch(() => res.status(401).send({ status: 401, error: 'You do not have permission to access this.' }));
+      .catch(() => res.status(401).send({ status: 401, error: 'You do not have permission to access this resource.' }));
   }
 
   static isUser(req, res, next) {
@@ -91,9 +91,9 @@ class UserMiddleware {
         if (user.id) {
           return next();
         }
-        return res.status(401).send({ status: 401, error: 'You do not have permission to access this.' });
+        return res.status(401).send({ status: 401, error: 'You do not have permission to access this resource.' });
       })
-      .catch(() => res.status(401).send({ status: 401, error: 'You do not have permission to access this.' }));
+      .catch(() => res.status(401).send({ status: 401, error: 'You do not have permission to access this resource.' }));
   }
 }
 
