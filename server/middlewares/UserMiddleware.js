05187425da8e18c17c3518ac6d36bfde30071877
frankly034 <franklynugege@gmail.com>
2019-02-24 15:43:39 +0100

    bugfix(user): Implement review feedback
    - handle more specific validations
    - returned more appropriate error messages

--- server/middlewares/UserMiddleware.js
+++ server/middlewares/UserMiddleware.js
@@ -2,53 +2,116 @@ import TokenModel from '../models/TokenModel';
 
 class UserMiddleware {
   static create(req, res, next) {
-    let error;
-    const {
-      email, phoneNumber, firstname, lastname, passportUrl, password,
-    } = req.body;
+    let validationState = false;
+    let field;
+    const requiredFields = [
+      { name: 'email', type: 'email' },
+      { name: 'phoneNumber', type: 'phoneNumber' },
+      { name: 'firstname', type: 'string' },
+      { name: 'lastname', type: 'string' },
+      { name: 'password', type: 'any' },
+      { name: 'passportUrl', type: 'string' },
+    ];
 
-    if (!email.trim() || !phoneNumber.trim() || !firstname.trim() || !lastname.trim() || !passportUrl.trim() || !password.trim()) {
-      error = { error: 'Bad request: Missing field (email, phone, firstname, lastname, passport url and password are required)', status: 400 };
-      return res.status(400).send(error);
-    }
-    
-    if (!UserMiddleware.validateString(passportUrl)) {
-      error = { error: 'Bad request: passport url must be a string', status: 400 };
-      return res.status(400).send(error);
-    }
+    for (let i = 0; i < requiredFields.length; i += 1) {
+      field = requiredFields[i];
+      if (!req.body[field.name]) {
+        validationState = true;
+        return res.status(422).send({
+          error: `Unprocessable Entity: Missing ${field.name}, ${field.name} is required`,
+          status: 422,
+        });
+      }
+      if (!req.body[field.name].trim()) {
+        validationState = true;
+        return res.status(422).send({
+          error: `Unprocessable Entity: Empty ${field.name}, ${field.name} cannot be empty`,
+          status: 422,
+        });
+      }
 
-    if (!UserMiddleware.validateString(firstname) || !UserMiddleware.validateString(lastname)) {
-      error = { error: 'Bad request: Invalid name field types', status: 400 };
-      return res.status(400).send(error);
-    }
+      if (field.name === 'password' && req.body.password.trim().length < 6) {
+        validationState = true;
+        return res.status(422).send({
+          error: `Unprocessable Entity: ${field.name} must be longer than 6 characters`,
+          status: 422,
+        });
+      }
 
-    if (!UserMiddleware.validateEmail(email) || !UserMiddleware.validatePhone(phoneNumber)) {
-      error = { error: 'Bad request: Invalid field types (email or phone)', status: 400 };
-      return res.status(400).send(error);
+      switch (field.type) {
+        case 'phoneNumber':
+          if (!UserMiddleware.validatePhone(req.body[field.name])) {
+            validationState = true;
+            return res.status(422).send({
+              error: `Unprocessable Entity: ${field.name} is invalid, requires a valid phone number`,
+              status: 422,
+            });
+          }
+          break;
+        case 'email':
+          if (!UserMiddleware.validateEmail(req.body[field.name])) {
+            validationState = true;
+            return res.status(422).send({
+              error: `Unprocessable Entity: ${field.name} is invalid requires a valid email.`,
+              status: 422,
+            });
+          }
+          break;
+        case 'string':
+          if (!UserMiddleware.validateString(req.body[field.name])) {
+            validationState = true;
+            return res.status(422).send({
+              error: `Unprocessable Entity: ${field.name} is invalid, requires a valid string.`,
+              status: 422,
+            });
+          }
+          break;
+        default: break;
+      }
     }
-
-    if ((password.trim()).length < 6) {
-      error = { error: 'Bad request: Password must be longer than 6 characters', status: 400 };
-      return res.status(400).send(error);
+    if (!validationState) {
+      return next();
     }
-
-    return next();
   }
 
   static login(req, res, next) {
-    let error;
+    let validationState = false;
+    let field;
+    const requiredFields = [
+      { name: 'email', type: 'email' },
+      { name: 'password', type: 'any' },
+    ];
 
-    if (!UserMiddleware.validateEmail(req.body.email)) {
-      error = { error: 'Bad request: Invalid email field', status: 400 };
-      return res.status(400).send(error);
-    }
-    
-    if (!req.body.email.trim() || !req.body.password.trim()) {
-      error = { error: 'Bad request: Missing field (email and password are required)', status: 400 };
-      return res.status(400).send(error);
-    }
+    for (let i = 0; i < requiredFields.length; i += 1) {
+      field = requiredFields[i];
+      if (!req.body[field.name]) {
+        validationState = true;
+        return res.status(422).send({
+          error: `Unprocessable Entity: Missing ${field.name}, ${field.name} is required`,
+          status: 422,
+        });
+      }
+      if (!req.body[field.name].trim()) {
+        validationState = true;
+        return res.status(422).send({
+          error: `Unprocessable Entity: Empty ${field.name}, ${field.name} cannot be empty`,
+          status: 422,
+        });
+      }
 
-    return next();
+      if (field.type === 'email') {
+        if (!UserMiddleware.validateEmail(req.body[field.name])) {
+          validationState = true;
+          return res.status(422).send({
+            error: `Unprocessable Entity: ${field.name} is invalid requires a valid email.`,
+            status: 422,
+          });
+        }
+      }
+    }
+    if (!validationState) {
+      return next();
+    }
   }
 
   static validateEmail(email) {
@@ -70,18 +133,6 @@ class UserMiddleware {
       return res.status(400).send(error);
     }
     return next();
-  }
-
-  static isAdmin(req, res, next) {
-    const token = req.header('x-auth');
-    TokenModel.decodeToken(token)
-      .then((user) => {
-        if (user.isAdmin) {
-          return next();
-        }
-        return res.status(401).send({ status: 401, error: 'You do not have permission to access this resource.' });
-      })
-      .catch(() => res.status(401).send({ status: 401, error: 'You do not have permission to access this resource.' }));
   }
 
   static isUser(req, res, next) {
